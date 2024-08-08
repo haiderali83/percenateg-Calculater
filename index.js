5a8e1b5a8b22d9a2b402c29c3c745bd4602100bb
haider <meharhaider83@gmail.com>
2024-08-08 13:42:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function calculate(){
+    let sub1 = parseInt(document.getElementById('sub1').value);
+    let sub2 = parseInt(document.getElementById('sub2').value);
+    let sub3 = parseInt(document.getElementById('sub3').value); 
+    let sub4 = parseInt(document.getElementById('sub4').value); 
+    let sub5 = parseInt(document.getElementById('sub5').value); 
+    let tmMarks= document.getElementById('tm1').value;
+    let sum = sub1 + sub2 + sub3 + sub4 + sub5 
+    let res = (sum/tmMarks )* 100
+    alert(res + '%' )
+
+    if (res >= 40 ){
+        alert('you have passed')
+    
+    }
+    else
+         alert('you are failed') 
+    }
+    
